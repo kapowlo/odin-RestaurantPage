2b018c69cafb8e58e1431222212b624aee1f99e7
mano <rsmike91@outlook.com>
2024-04-09 23:39:37 -0400

    Added style.css file style.css file is imported from index.js file
    modified webpack.config.js file by including module object

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,5 @@
 import { pageLoad } from "./initialPage-load";
+import './style.css';
 
 console.log("Hello people! ");
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -8,4 +8,12 @@ module.exports={
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [new HtmlWebpackPlugin({template: path.resolve(__dirname, 'src', 'index.html')})],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
 };
