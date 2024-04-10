5863dd6ecb65a8b4535ca7bf0e25f62856aa88af
mano <rsmike91@outlook.com>
2024-04-09 23:16:08 -0400

    Removed content inside div id content
    added initialPage-load.js file
    i exported the function pageLoad
    and will import into index.js file

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,5 @@
+import { pageLoad } from "./initialPage-load";
+
 console.log("Hello people! ");
+
+pageLoad();
---
+++ webpack/src/initialPage-load.js
@@ -0,0 +1,25 @@
+export function pageLoad(){
+    const myContentDiv = document.querySelector("#content")
+
+    const myH1header = document.createElement("h1");
+    myH1header.textContent="Welcome to Kapowlo's Restaurant!";
+    myH1header.classList.add("big-text")
+    myContentDiv.appendChild(myH1header);
+
+    const myRestaurantImage = document.createElement("img");
+    myRestaurantImage.src="../src/myAssets/restaurant-bg.jpg";
+    myRestaurantImage.alt="the inside of a beautiful restaurant";
+    myRestaurantImage.style.width="600px";
+    myRestaurantImage.style.height="600px";
+    myContentDiv.appendChild(myRestaurantImage);
+
+    const firstPara = document.createElement("p")
+    firstPara.classList.add("small-text")
+    firstPara.textContent="This is one of the most popular restaurant on earth, the customer service is impeccable,the food is delicious";
+
+    const secondPara = document.createElement("p");
+    secondPara.classList.add("small-text")
+    secondPara.textContent="Please consider eating at Kapowlo's restaurant and bring as many people as you want!";
+
+    myContentDiv.append(firstPara,secondPara);
+}
